c067b980f9d733dc679d4cc13b528e3dd46df1b8
Marco Antonio <lanaalves291@gmail.com>
2025-10-06 15:06:03 -0300

    baiaController pronto

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { login } = require('./authController');
+const { baias } = require('./baiaController');
 
 const app = express();
 const PORT = 4000;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/api/login', login);
+app.post('/api/baias', baias);
 
 app.listen(PORT, () => {
   console.log(`🚀 Backend rodando em http://localhost:${PORT}`);
